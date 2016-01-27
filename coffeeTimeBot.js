75d444acf4d7a64f9add0a057e6d9e1a9a17d2c1
kevincoleman <kevin.n.coleman@gmail.com>
2016-01-27 12:28:54 -0800

    fixed syntax

--- coffeeTimeBot.js
+++ coffeeTimeBot.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next){
   imageSearch.getImagesArray('coffee').done(function(img){
     var coffeePic = img[Math.floor(Math.random() * img.length) + 1];
     var botPayload = {
-      text: '![It’s Coffee Time.]('coffeePic')',
+      text: '![It’s Coffee Time.](' + coffeePic + ')',
       mrkdwn: true
     }
 
